eb9cbffb70feb328c5ebb0fd45c5e163e5b1b156
doraeball22 <anurak_b@cmu.ac.th>
2017-01-23 03:22:29 +0700

    เพิ่ม content

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
   // url: 'mongodb://bondev-na343ke6.cloudapp.net/expressauth',
-  // url: 'mongodb://0.0.0.0/expressauth',
+  // url: 'mongodb://localhost/expressauth',
   url: 'mongodb://mongo:27017/expressauth',
 };
