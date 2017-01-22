a103a654edf560aea32b813f1fb170da09686ec8
doraeball22 <anurak_b@cmu.ac.th>
2017-01-22 21:40:09 +0700

    first commit

---
+++ config/auth.js
@@ -0,0 +1,17 @@
+module.exports = {
+  facebookAuth: {
+    clientID: '452187178503318',
+    clientSecret: '17d0ff71021f2ff98c28606f639aa6fd',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+  },
+  twitterAuth: {
+    consumerKey: 'sOM9aZpoEyNAXKqvEUHd2O7cN',
+    consumerSecret: '6I1x0PgH41AxuqtAKcfRqYtWDMKNv0TBaaoegW3RP3o1ks7NPk',
+    callbackURL: 'http://localhost:3000/auth/twitter/callback',
+  },
+  googleAuth: {
+    clientID: '262575533252-sviar9bm39g5c4dnlb6erkpvjme4hit2.apps.googleusercontent.com',
+    clientSecret: '1K1fRgOmy1WJK4_Z8ycGRyuz',
+    callbackURL: 'http://localhost:3000/auth/google/callback',
+  },
+};
---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // url: 'mongodb://bondev-na343ke6.cloudapp.net/expressauth',
+  url: 'mongodb://localhost/expressauth',
+};
